b66e413d84afd267686bbfcfd86e6af0de43bd03
Zsolt Neszter <zneszter@gmail.com>
2023-06-08 12:31:43 +0200

    Karakter osztály létrehozása

---
+++ Karakter.java
@@ -0,0 +1,33 @@
+public class Karakter {
+
+    int elet;
+    String nev;
+    int pozicio;
+
+    public Karakter() {
+    }
+
+    public int getElet() {
+        return elet;
+    }
+
+    public void setElet(int elet) {
+        this.elet = elet;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public int getPozicio() {
+        return pozicio;
+    }
+
+    public void setPozicio(int pozicio) {
+        this.pozicio = pozicio;
+    }
+}
